ea09d82d8f4d54f42829f8b1157297db1e0dcc24
vosxvo <thonsai@outlook.com>
2021-04-15 12:18:05 +0700

    Adding Login fxml, css

---
+++ src/com/vosxvo/java/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package com.vosxvo.java.controllers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    public TextField username;
+    public PasswordField password;
+    public Button forgotPassword;
+    public Button signIn;
+    public Button createAccount;
+
+
+}
